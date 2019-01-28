8807620b64f186be74c713ff45c66d1b6754c33b
Stephen Anderson <anderssj@usc.edu>
2019-01-28 11:01:49 -0800

    config seutp with new user mongo DB

--- config.js
+++ config.js
@@ -5,10 +5,9 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
     process.env.DATABASE_URL ||
-    'mongodb://stevezg:a000000@ds115595.mlab.com:15595/spaced-repitition',
+    'mongodb://user:a000000@ds115595.mlab.com:15595/spaced-repitition',
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/thinkful-backend-test',
-
   JWT_SECRET: process.env.JWT_SECRET
 }
