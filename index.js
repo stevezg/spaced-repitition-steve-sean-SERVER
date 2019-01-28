ab99476b9818600dad3bbd839f9ed93746c47f6b
Stephen Anderson <anderssj@usc.edu>
2019-01-28 11:06:15 -0800

    not good

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-const {router: userRouter} = require('./users');
-const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
+const { PORT, CLIENT_ORIGIN } = require('./config')
+const { dbConnect } = require('./db-mongoose')
+const { router: userRouter } = require('./users')
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth')
 
-const app = express();
+const app = express()
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
-);
+)
 
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
-);
-passport.use(localStrategy);
-passport.use(jwtStrategy);
-app.use('/users/', userRouter);
-app.use('/auth/', authRouter);
-const jwtAuth = passport.authenticate('jwt', { session: false });
+)
+passport.use(localStrategy)
+passport.use(jwtStrategy)
+app.use('/api/users/', userRouter)
+app.use('/api/auth/', authRouter)
+const jwtAuth = passport.authenticate('jwt', { session: false })
 
-app.use('/api/users', usersRouter);
+app.use('/api/users', usersRouter)
 
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
+      console.info(`App listening on port ${server.address().port}`)
     })
     .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+      console.error('Express failed to start')
+      console.error(err)
+    })
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+  dbConnect()
+  runServer()
 }
 
-module.exports = { app };
+module.exports = { app }
