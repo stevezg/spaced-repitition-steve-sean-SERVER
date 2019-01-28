dcea629b05b9d6f68e1e9b264aad9c3ba43111c9
Stephen Anderson <anderssj@usc.edu>
2019-01-28 10:53:20 -0800

    adds JWT and passport to index

--- index.js
+++ index.js
@@ -1,41 +1,47 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
-const { PORT, CLIENT_ORIGIN } = require('./config');
-const { dbConnect } = require('./db-mongoose');
-// const {dbConnect} = require('./db-knex');
+const { PORT, CLIENT_ORIGIN } = require('./config')
+const { dbConnect } = require('./db-mongoose')
+const userRouter = require('./users')
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth')
 
-const app = express();
+const app = express()
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
     skip: (req, res) => process.env.NODE_ENV === 'test'
   })
-);
+)
 
 app.use(
   cors({
     origin: CLIENT_ORIGIN
   })
-);
+)
+passport.use(localStrategy)
+passport.use(jwtStrategy)
+app.use('/users/', userRouter)
+app.use('/auth/', authRouter)
+const jwtAuth = passport.authenticate('jwt', { session: false })
 
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
-      console.info(`App listening on port ${server.address().port}`);
+      console.info(`App listening on port ${server.address().port}`)
     })
     .on('error', err => {
-      console.error('Express failed to start');
-      console.error(err);
-    });
+      console.error('Express failed to start')
+      console.error(err)
+    })
 }
 
 if (require.main === module) {
-  dbConnect();
-  runServer();
+  dbConnect()
+  runServer()
 }
 
-module.exports = { app };
+module.exports = { app }
