b662a2cc7a0d8359574e9396cdfafe17b000024f
seankrummel <sean.krummel@gmail.com>
2019-01-30 10:46:13 -0600

    added jwt authentication to the router

--- words/router.js
+++ words/router.js
@@ -1,7 +1,10 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const {User} = require('../users');
+
+router.use('/', passport.authenticate('jwt', {session: false, failWithError: true}));
 
 router.get('/', (req, res, next) => {
   const userId = req.user.id;
